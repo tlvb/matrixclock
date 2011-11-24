403eae99499b0231fc3f7f196f5577b08fc339db
Leo Bärring <leo@patternmaker>
2011-11-24 20:35:04 +0100

    binary counter working
    Signed-off-by: Leo Bärring <leo@patternmaker>

--- main.c
+++ main.c
@@ -1,43 +1,7 @@
-#if 0
-
-use warnings;
-use strict;
-use Tie::File;
-
-my $bitmap = {};
-$bitmap->{0} = "        l         b     ";
-$bitmap->{1} = "   t  lll       bbb     ";
-$bitmap->{2} = "  tt   ll        bb     ";
-$bitmap->{3} = "  tt   ll vv  vv bbbbbb ";
-$bitmap->{4} = "tttttt ll v   vv bb   bb";
-$bitmap->{5} = "  tt   ll v   vv bb 0 bb";
-$bitmap->{6} = "  tt   ll  v vv  bb   bb";
-$bitmap->{7} = "  t    lll  vv  bbbbbbb ";
-
-my @cases;
-for my $col (0..23) {
-    my $data = 0;
-    for my $row (0..7) {
-        $data |= 2**$row if (substr($bitmap->{$row},$col,1) ne ' ');
-    }
-    push @cases, sprintf "case %d: displaydata = 0x%x; break;\n", $col, $data;
-}
-
-tie my @f, 'Tie::File', $0 or die $!;
-
-my ($bl, $el) = (-1, -1);
-for (0..$#f) {
-    $bl = $_+1 if $f[$_] =~ m.^// BEGIN PERL_GENERATED.;
-    $el = $_-1 if $f[$_] =~ m.^// END PERL_GENERATED. and $bl > 0;
-    last if $el > 0;
-}
-splice @f, $bl, $el-$bl+1, @cases;
-untie @f;
-=pod
-#endif
 #define F_CPU 20000000
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdint.h>
 
@@ -58,45 +22,32 @@ void display_seed(void);
 void display_next(void);
 void display_writecol(uint8_t c);
 
+volatile uint8_t column = 0;
+volatile uint16_t subseconds = 0;
+volatile uint8_t seconds = 0, minutes = 0, hours = 0;
 int main(void) {
+
+    DDRB |= _BV(PB1);
+
+    TCCR1B = _BV(WGM12) | _BV(CS10);
+    TIMSK1 = _BV(OCIE1A);
+    OCR1AH = 0x0f;
+    OCR1AL = 0xa0;
+    sei();
+
     display_setup();
-    
+    uint8_t displaydata = 0x0f;
+
     for (;;) {
-        display_seed();
-        for (uint8_t i=0; i<24; ++i) {
-            uint8_t displaydata;
-            switch (i) {
-// BEGIN PERL_GENERATED
-case 0: displaydata = 0x10; break;
-case 1: displaydata = 0x10; break;
-case 2: displaydata = 0xfc; break;
-case 3: displaydata = 0x7e; break;
-case 4: displaydata = 0x10; break;
-case 5: displaydata = 0x10; break;
-case 6: displaydata = 0x2; break;
-case 7: displaydata = 0xfe; break;
-case 8: displaydata = 0xff; break;
-case 9: displaydata = 0x80; break;
-case 10: displaydata = 0x38; break;
-case 11: displaydata = 0x48; break;
-case 12: displaydata = 0x80; break;
-case 13: displaydata = 0xc0; break;
-case 14: displaydata = 0x78; break;
-case 15: displaydata = 0x38; break;
-case 16: displaydata = 0x82; break;
-case 17: displaydata = 0xfe; break;
-case 18: displaydata = 0xff; break;
-case 19: displaydata = 0x88; break;
-case 20: displaydata = 0xa8; break;
-case 21: displaydata = 0x88; break;
-case 22: displaydata = 0xf8; break;
-case 23: displaydata = 0x70; break;
-// END PERL_GENERATED
-            }
-            display_writecol(displaydata);
-            _delay_us(500);
-            display_next();
-        }
+        if (column > 0 && column < 7)
+            displaydata = hours;
+        else if (column > 8 && column < 15) 
+            displaydata = minutes;
+        else if (column > 16 && column < 23) 
+            displaydata = seconds;
+        else
+            displaydata = 0;
+        display_writecol(displaydata);
     }
 }
 
@@ -104,7 +55,6 @@ void display_setup(void) {
     DDRC |= 0x0f | _BV(ROWCLOCK) | _BV(ROWCLEAR);
     DDRD |= 0xff | _BV(ROWSEED);
     PORTC |= _BV(ROWCLEAR); // pull rowclear high and out of reset
-    _delay_ms(250);
 }
 
 void display_seed(void) {
@@ -125,6 +75,27 @@ void display_writecol(uint8_t c) {
     PORTD &= 0x0f;
     PORTD |= (c & 0xf0);
 }
-#if 0
-=cut
-#endif
+
+ISR(TIMER1_COMPA_vect) {
+
+    PINB |= _BV(PB1);
+    if (++subseconds == 5000) {
+        subseconds = 0;
+        if (++seconds == 60) {
+            seconds = 0;
+            if (++minutes == 60) {
+                minutes = 0;
+                if (++hours == 0) {
+                    hours = 0;
+                }
+            }
+        }
+    }
+    if (++column == 24) {
+        column = 0;
+        display_seed();
+    }
+    else {
+        display_next();
+    }
+}
--- test.pl
+++ test.pl
@@ -5,22 +5,22 @@ use strict;
 use Tie::File;
 
 my $bitmap = {};
-$bitmap->{0} = "        l         b     ";
-$bitmap->{1} = "   t  lll       bbb     ";
-$bitmap->{2} = "  tt   ll        bb     ";
-$bitmap->{3} = "  tt   ll        bbbbbb ";
-$bitmap->{4} = "tttttt ll vv  vv bb   bb";
-$bitmap->{5} = "  tt   ll v   vv bb   bb";
-$bitmap->{6} = "  tt   ll  v vv  bb   bb";
-$bitmap->{7} = "  t    lll  vv  bbbbbbb ";
+$bitmap->{0} = " 000    1    222   333  4   4 55555   66  77777  888   999  ";
+$bitmap->{1} = "0   0 111   2   2 3   3 4   4 5      6        7 8   8 9   9 "; 
+$bitmap->{2} = "0  00   1       2     3 4   4 5     6         7 8   8 9   9 ";
+$bitmap->{3} = "0 0 0   1      2   333  44444 5555  6666     7   888   9999 ";
+$bitmap->{4} = "00  0   1     2       3     4     5 6   6   7   8   8     9 ";
+$bitmap->{5} = "00  0   1    2    3   3     4     5 6   6   7   8   8    9  ";
+$bitmap->{6} = " 000  11111 22222  333      4 5555   666    7    888   99   ";
+$bitmap->{7} = "                                                            ";
 
-my @cases;
+my @columns;
 for my $col (0..23) {
     my $data = 0;
     for my $row (0..7) {
         $data |= 2**$row if (substr($bitmap->{$row},$col,1) ne ' ');
     }
-    push @cases, sprintf "case %d: display_writecol(0x%x); break;\n", $col, $data;
+    push @columns, sprintf "0x%02x", $data;
 }
 
 tie my @f, 'Tie::File', $0 or die $!;
@@ -31,12 +31,12 @@ for (0..$#f) {
     $el = $_-1 if $f[$_] =~ m.^// END PERL_GENERATED. and $bl > 0;
     last if $el > 0;
 }
-print "$bl $el\n";
-splice @f, $bl, $el-$bl+1, @cases;
+splice @f, $bl, $el-$bl+1, ("uint8_t font[] = {".join(', ',@columns)."};\n");
 untie @f;
 =pod
 #endif
 // BEGIN PERL_GENERATED
+uint8_t font[] = {0x3e, 0x71, 0x49, 0x45, 0x3e, 0x00, 0x42, 0x42, 0x7f, 0x40, 0x40, 0x00, 0x42, 0x61, 0x51, 0x49, 0x46, 0x00, 0x22, 0x49, 0x49, 0x49, 0x36, 0x00};
 // END PERL_GENERATED
 #ifdef 0
 =cut
